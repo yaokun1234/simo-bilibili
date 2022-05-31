cca38b9497f13a60398cdf0999782ce699094783
simo <2921847833@qq.com>
2022-05-31 09:35:03 +0800

    初始化项目

---
+++ simo-bilibili-api/src/main/java/com/simo/BilibiliApplication.java
@@ -0,0 +1,12 @@
+package com.simo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class BilibiliApplication {
+    public static void main(String[] args) {
+        ConfigurableApplicationContext app = SpringApplication.run(BilibiliApplication.class, args);
+    }
+}
